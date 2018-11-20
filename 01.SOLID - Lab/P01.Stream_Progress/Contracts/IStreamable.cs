7c83034815eb57a05b0745fc970272be4269d911
Iliyan Todorov <venomflow2018@gmail.com>
2018-11-20 20:53:10 +0200

    SOLID LAB

---
+++ 01.SOLID - Lab/P01.Stream_Progress/Contracts/IStreamable.cs
@@ -0,0 +1,9 @@
+﻿namespace P01.Stream_Progress.Contracts
+{
+    public interface IStreamable
+    {
+        int Length { get; set; }
+
+        int BytesSent { get; set; }
+    }
+}
---
+++ 01.SOLID - Lab/P01.Stream_Progress/StreamProgressInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using P01.Stream_Progress.Contracts;
+
+namespace P01.Stream_Progress
+{
+    public class StreamProgressInfo : IStreamable
+    {
+        private IStreamable streamable;
+
+        // If we want to stream a music file, we can't
+        public StreamProgressInfo(IStreamable streamable)
+        {
+            this.streamable = streamable;
+        }
+
+        public int CalculateCurrentPercent()
+        {
+            return (this.streamable.BytesSent * 100) /
+                   this.streamable.Length;
+        }
+
+        public int Length { get; set; }
+
+        public int BytesSent { get; set; }
+    }
+}
