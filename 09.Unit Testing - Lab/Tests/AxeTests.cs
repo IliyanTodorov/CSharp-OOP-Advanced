83424c2208207b1d3409eebf8eb5bb44dcde7564
Iliyan Todorov <venomflow2018@gmail.com>
2018-12-09 10:57:01 +0200

    UNIT TESTING - LAB

---
+++ 09.Unit Testing - Lab/Tests/AxeTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        private Axe weapon;
+        private Dummy dummy;
+        private const int weaponAttack = 100;
+        private const int weaponDurability = 10;
+        private const int dummyHealth = 100;
+        private const int dummyXP = 20;
+
+        [SetUp]
+        public void CreateWeaponBeforeEachTest()
+        {
+            this.weapon = new Axe(weaponAttack, weaponDurability);
+            this.dummy = new Dummy(dummyHealth, dummyXP);
+        }
+
+        [Test]
+        public void TestIfWeaponLosesDurabilityAfterAttack()
+        {
+            this.weapon.Attack(this.dummy);
+
+            const int expectedResult = 9;
+            var actualResult = this.weapon.DurabilityPoints;
+
+            Assert.That(actualResult, Is.EqualTo(expectedResult), "Axe Durability doesn't change after attack.");
+        }
+
+        [Test]
+        public void AttackingWithABrokenWeapon()
+        {
+            this.weapon = new Axe(100, 0);
+
+            Assert.Throws<InvalidOperationException>(() => this.weapon.Attack(this.dummy), "Attacking with broken weapon must throw InvalidOperationException!");
+        }
+    }
+}
---
+++ 09.Unit Testing - Lab/Tests/DummyTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        private Dummy dummy;
+        private const int dummyHealth = 100;
+        private const int dummyXP = 10;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.dummy = new Dummy(dummyHealth, dummyXP);
+        }
+
+        [Test]
+        public void LosesHealthIfAttacked()
+        {
+            this.dummy.TakeAttack(9);
+
+            const int expectedResult = 91;
+
+            Assert.That(this.dummy.Health, Is.EqualTo(expectedResult), "Dummy Health doesn't change after taking damage");
+        }
+
+        [Test]
+        public void DummyThrowsAnExceptionIfAttacked()
+        {
+            this.dummy.TakeAttack(140);
+
+            const int expectedResult = -40;
+
+            Assert.That(dummy.Health, Is.EqualTo(expectedResult));
+            Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(1), "Dummy must throw exception when attacked!");
+        }
+
+        [Test]
+        public void DeadDummyCanGiveXp()
+        {
+            this.dummy.TakeAttack(120);
+
+            const int expectedResult = 10;
+
+            Assert.That(dummy.GiveExperience(), Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void AliveDummyCanGiveXp()
+        {
+            Assert.Throws<InvalidOperationException>(() => this.dummy.GiveExperience(), "Alive dummy must throw exception when try to give XP");
+        }
+    }
+}
